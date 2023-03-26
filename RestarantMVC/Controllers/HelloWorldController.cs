da452f6074be5d601e94fa38a29958027f3c2e1c
Zyadachi <ozdowy.adam8@gmail.com>
2023-03-27 00:22:11 +0200

    Adding "Create List"

--- RestarantMVC/Controllers/HelloWorldController.cs
+++ RestarantMVC/Controllers/HelloWorldController.cs
@@ -1,12 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RestarantMVC.Models;
 
 namespace RestarantMVC.Controllers
 {
     public class HelloWorldController : Controller
     {
+        private static List<DogViewModel> dogs = new List<DogViewModel>();
         public IActionResult Index()
         {
-            return View();
+            return View(dogs);
+        }
+
+        public IActionResult Create()
+        {
+            var dogVm = new DogViewModel();
+            return View(dogVm);
+        }
+
+        public IActionResult CreateDog(DogViewModel dogViewModel)
+        {
+            dogs.Add(dogViewModel);
+            return RedirectToAction(nameof(Index));
+            //return View("Index");
         }
 
         public string Hello()
